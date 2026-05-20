023673890c2bc9873fa7d89708ae8bc41f600d31
olsherif <92591038+olsherif@users.noreply.github.com>
2026-05-21 00:45:00 +0300

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,13 @@
+import logging
+import sys
 
+def setup_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
