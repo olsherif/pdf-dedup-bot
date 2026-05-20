62a933c0c34fd8dc460a2e2134eab6f01fb6b776
olsherif <92591038+olsherif@users.noreply.github.com>
2026-05-21 01:33:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import asyncio
 from dotenv import load_dotenv
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 
-from bot.handlers import start, handle_pdf
+from bot.handlers import start, handle_video
 from utils.keep_alive import start_web_server
 from utils.logger import setup_logger
 
@@ -15,18 +15,17 @@ PORT = int(os.getenv("PORT", 8000))
 logger = setup_logger(__name__)
 
 async def main():
-    # تشغيل سيرفر الصحة في Thread منفصل
     start_web_server()
 
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.Document.PDF, handle_pdf))
+    # استقبال الفيديوهات فقط
+    app.add_handler(MessageHandler(filters.VIDEO, handle_video))
 
-    logger.info("Starting bot...")
+    logger.info("Bot started...")
     await app.initialize()
     await app.start()
     await app.updater.start_polling()
-    # البوت شغال للأبد
     while True:
         await asyncio.sleep(3600)
 
